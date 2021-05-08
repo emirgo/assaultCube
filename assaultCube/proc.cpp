97f6dac9339a0f80688d2f9338b1d19e05b13c0c
Emir <40762681+emirgo@users.noreply.github.com>
2021-05-08 17:47:55 +0200

    FindDMAAddy

--- assaultCube/proc.cpp
+++ assaultCube/proc.cpp
@@ -49,7 +49,13 @@ uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 	return modBaseAddr;
 }
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int>)
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
 {
-
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i)
+	{
+		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), NULL);
+		addr += offsets[i];
+	}
+	return addr;
 }
--- assaultCube/proc.h
+++ assaultCube/proc.h
@@ -5,4 +5,4 @@
 
 DWORD GetProcId(const wchar_t* procName);
 uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int>);
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
